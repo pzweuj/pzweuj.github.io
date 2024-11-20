aed7234d0917b283fd433339df97820325464e23
EuJ <pzweuj@live.com>
2024-11-20 23:31:48 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // 添加这行
+  // output: 'export',  // 添加这行
   basePath: '/pzweuj.github.io',
   images: {
     unoptimized: true,

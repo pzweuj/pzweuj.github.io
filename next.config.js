7f954565b41dce4ad9a09534db14c3845fbc1c88
EuJ <pzweuj@live.com>
2024-11-21 23:13:20 +0800

    降级nextjs版本到14.1.0

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 你原来的配置内容
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // 其他配置...
+}
+
+module.exports = nextConfig 

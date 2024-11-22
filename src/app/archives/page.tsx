e4d1f3802374a65f936ec30dba0b3671ef69beba
EuJ <pzweuj@live.com>
2024-11-22 22:07:32 +0800

    202411222207

--- src/app/archives/page.tsx
+++ src/app/archives/page.tsx
@@ -6,15 +6,9 @@ export default async function ArchivesPage() {
   
   return (
     <div className="max-w-4xl mx-auto px-4 py-12">
-      {/* <div className="text-center mb-8">
-        <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
-          文章归档
-        </h1>
-        <p className="mt-2 text-gray-600 dark:text-gray-400">
-          共 {posts.length} 篇文章
-        </p>
-      </div> */}
-      
+      <h1 className="text-3xl font-bold mb-8 text-gray-900 dark:text-gray-100">
+        归档
+      </h1>
       <ArchivesList initialPosts={posts} />
     </div>
   )

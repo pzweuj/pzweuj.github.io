3c3a575c19dd2694da3916dbd5b1baa31c678f77
EuJ <pzweuj@live.com>
2024-11-23 13:41:49 +0800

    更新到v0.4.1

--- src/lib/plugins/remarkQQMusic.ts
+++ src/lib/plugins/remarkQQMusic.ts
@@ -1,6 +1,6 @@
 import { visit } from 'unist-util-visit'
 import type { Plugin } from 'unified'
-import type { Root } from 'mdast'
+import type { Root, Paragraph, Text } from 'mdast'
 
 interface QQMusicOptions {
   className?: string
@@ -12,14 +12,23 @@ export const remarkQQMusic: Plugin<[QQMusicOptions?], Root> = (options = {}) => 
   const className = options.className || 'qq-music-player'
   
   return (tree) => {
-    visit(tree, 'paragraph', (node: any) => {
-      if (node.children.length === 1 && node.children[0].type === 'text') {
-        const match = node.children[0].value.match(QQ_MUSIC_REGEX)
+    visit(tree, 'paragraph', (node: Paragraph) => {
+      if (
+        node.children.length === 1 && 
+        node.children[0].type === 'text'
+      ) {
+        const textNode = node.children[0] as Text
+        const match = textNode.value.match(QQ_MUSIC_REGEX)
         if (match) {
           const songId = match[1]
-          node.type = 'html'
-          node.children = undefined
-          node.value = `<div class="${className}"><iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="100%" height="86" src="https://i.y.qq.com/n2/m/outchain/player/index.html?songid=${songId}"></iframe></div>`
+          const htmlNode = node as unknown as { 
+            type: 'html'
+            value: string
+            children?: never
+          }
+          htmlNode.type = 'html'
+          htmlNode.children = undefined
+          htmlNode.value = `<div class="${className}"><iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="100%" height="86" src="https://i.y.qq.com/n2/m/outchain/player/index.html?songid=${songId}"></iframe></div>`
         }
       }
     })
